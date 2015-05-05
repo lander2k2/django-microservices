6386b42fb20eb7875105df5dab100b71a4ddfc07
Richard Lander <richard.lander@tribridge.com>
2015-05-04 22:00:58 -0400

    setup edit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='django-microservices',
       version='0.1',
       description='A framework for building microservices with Django.',
-      url='http://github.com/lander2k2/django-microservices',
+      url='https://github.com/lander2k2/django-microservices',
       author='Richard Lander',
       author_email='lander2k2@gmail.com',
       license='MIT',
